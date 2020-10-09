41a3124f4a7c18192ea1de61372694feaec6fd7a
Expedito Andrade <expeditojazz@gmail.com>
2020-10-09 14:53:34 -0300

    fix typo

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ const resolvers = require('./resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => console.log(`Server ready at ${url}`).catch((error) => console.log("Server failed:", error))
+server
+  .listen()
+  .then(({ url }) => console.log(`Server ready at ${url}`))
+  .catch((error) => console.log('Server failed:', error));
